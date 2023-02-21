c13aa01d54648d585099686cb81199b24d13fccd
FelipePatriota <felipe.patriota@ufrpe.br>
2023-02-21 11:13:43 -0300

    implementando adicionador de conteudo no csv

--- extratorDadosUf.py
+++ extratorDadosUf.py
@@ -16,10 +16,10 @@ site = requests.get(url, headers=headers)
 soup = BeautifulSoup(site.content, 'html.parser')
 
 # criando um dicionário para armazenar os dados que serão extraídos
-quero = {'link': [], 'data': [], 'titulo': []}
+quero = {'link': [], 'data': [], 'titulo': [], 'texto': []}
 
 # pegando o número de páginas
-for i in range(0, 3):
+for i in range(0,3):
     url_pag = f'https://www.ufrpe.br/br/lista-de-noticias?page={i}'
     site = requests.get(url_pag, headers=headers)
     soup = BeautifulSoup(site.content, 'html.parser')
@@ -41,6 +41,20 @@ for i in range(0, 3):
     for titulo in t:
         titulo = titulo.find('a').get_text().strip()
         quero['titulo'].append(titulo)
+    c = soup.find_all('div', class_=re.compile('views-field views-field-body'))
+
+
+numero = len (quero['link'])
+for i in range(numero):
+    url_p = 'https://www.ufrpe.br/' + quero['link'][i]	
+    site = requests.get(url_p, headers=headers)
+    soup = BeautifulSoup(site.content, 'html.parser')
+    conteudo = soup.find('div', class_=re.compile('field field-name-body field-type-text-with-summary field-label-hidden'))
+    texto = conteudo.find('p').get_text().strip()
+    quero['texto'].append(texto)
+    
+
+
 
 df = pd.DataFrame(quero)
 df.to_csv('dados_uf.csv', index=False)
