a7f4dfca67f288289c5ba57361ee101f82479576
FelipePatriota <felipe.patriota@ufrpe.br>
2023-03-24 19:39:52 -0300

    atualização

--- extratorDadosUf.py
+++ extratorDadosUf.py
@@ -2,64 +2,54 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 import re
-import math
-# url de onde será extraído os dados
+
 url = "https://www.ufrpe.br/br/lista-de-noticias"
 
-# cabeçalho para simular um navegador
 headers = {'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 \ (KHTML, like Gecko) Chrome / 86.6.4240.198Safari / 537.36"}
 
-# fazendo a requisição
 site = requests.get(url, headers=headers)
 
-# transformando o conteúdo em um objeto BeautifulSoup
 soup = BeautifulSoup(site.content, 'html.parser')
 
-# criando um dicionário para armazenar os dados que serão extraídos
 quero = {'link': [], 'data': [], 'titulo': [], 'texto': []}
 
 # pegando o número de páginas
-for i in range(0,3):
-    url_pag = f'https://www.ufrpe.br/br/lista-de-noticias?page={i}'
+for i in range(0,10):
+    url_pag = f'http://www.uabj.ufrpe.br/br/noticias?page={i}'
     site = requests.get(url_pag, headers=headers)
     soup = BeautifulSoup(site.content, 'html.parser')
     link = soup.find_all('div', class_=re.compile('views-field views-field-title'))
    
-    # for para pegar os links
     for l in link:
-        links = l.find('a').get('href')
-        quero['link'].append(links)    
-    d = soup.find_all('div', class_=re.compile('views-field-created'))
+        titulo = l.find('a').get_text().strip()
+        if 'bolsas' in titulo:
+            quero['titulo'].append(titulo)
+            links = l.find('a').get('href')
+            quero['link'].append(links)    
+        d = soup.find_all('div', class_=re.compile('views-field-created'))
    
-    # for para pegar a data
-    for dado in d:
-        data = dado.find('span', class_=re.compile('postado')).get_text().strip()
-        quero['data'].append(data)
-    t = soup.find_all('div', class_=re.compile('views-field views-field-title'))
+        for dado in d:
+            data = dado.find('span', class_=re.compile('postado'))
+            if data:
+                data = data.get_text().strip()
+                quero['data'].append(data)
+            else:
+                quero['data'].append("")
+
+            t = soup.find_all('div', class_=re.compile('views-field views-field-title'))
    
-    # for para pegar o título
-    for titulo in t:
-        titulo = titulo.find('a').get_text().strip()
-        quero['titulo'].append(titulo)
-    c = soup.find_all('div', class_=re.compile('views-field views-field-body'))
+            for titulo in t:
+                titulo = titulo.find('a').get_text().strip()
+                c = soup.find_all('div', class_=re.compile('views-field views-field-body'))
+                for div in c:
+                    soup = div.find('div', class_=re.compile('field field-name-body field-type-text-with-summary field-label-hidden'))
+                    if soup is not None:
+                        texto = soup.find_all('p')
+                        texto = [t.get_text().strip() for t in texto]
+                        texto = ' '.join(texto)
+                        quero['texto'].append(texto)
+                    else:
+                        quero['texto'].append(None)
 
-
-#for para pegar o texto
-for i in range(len (quero['link'])):
-    url_p = 'https://www.ufrpe.br/' + quero['link'][i]	
-    site = requests.get(url_p, headers=headers)
-    soup = BeautifulSoup(site.content, 'html.parser')
-    soup = soup.find('div', class_=re.compile('field field-name-body field-type-text-with-summary field-label-hidden'))
-    texto = soup.find_all('p')
-    texto = [t.get_text().strip() for t in texto]
-    texto = ' '.join(texto)
-    quero['texto'].append(texto)
-
-
-
-
-# transformando o dicionário em um DataFrame
 df = pd.DataFrame(quero)
-# salvando o DataFrame em um arquivo csv
 df.to_csv('dados_uf.csv', index=False)
-
---
+++ teste.py
@@ -0,0 +1,48 @@
+import openai
+
+# Substitua "sk-..." pela sua chave de API secreta
+openai.api_key = "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"
+
+# Escolha um modelo da open ai (por exemplo, davinci)
+model = "text-davinci-003"
+
+# Leia o arquivo CSV usando o módulo csv do python
+import csv
+data = []
+with open("Data.csv", encoding="utf-8") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        data.append(row[0])
+
+# Crie uma função que recebe uma pergunta do usuário e gera um texto usando o modelo da open ai
+def generate_text(question):
+    # Construa a entrada para a API da open ai com os dados do CSV e a pergunta do usuário
+    input = "Dados:\n"
+    for row in data:
+        input += row + "\n"
+    input += "\nPergunta: " + question + "\n\nResposta:"
+
+    # Envie uma requisição para a API da open ai com os parâmetros desejados
+    response = openai.Completion.create(
+        engine=model,
+        prompt=input,
+        temperature=0.5,
+        max_tokens=100,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+        stop=["\n"]
+    )
+
+    # Retorne a resposta gerada pelo modelo
+    return response["choices"][0]["text"]
+
+# Teste a função com uma pergunta de exemplo
+while True:
+
+    question = input("Digite uma pergunta: ")
+    if question == "quit":
+        break
+    answer = generate_text(question)
+    print(answer)
+
